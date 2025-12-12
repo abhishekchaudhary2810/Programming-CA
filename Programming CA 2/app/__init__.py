1e6d2eaa300b1ad4e69ca225a31434d4dacf1c4f
abhishekchaudhary2810 <abhisheksinghwall@gmail.com>
2025-12-12 17:26:36 +0000

    Add files via upload

---
+++ Programming CA 2/app/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from config import Config
+
+try:
+    import pymysql
+
+    pymysql.install_as_MySQLdb()
+except:
+    pass
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+
+    # Load default config
+    app.config.from_object(Config)
+
+    # Apply test configuration
+    if test_config:
+        app.config.update(test_config)
+
+    if app.config.get("TESTING"):
+        app.config["WTF_CSRF_ENABLED"] = False
+
+    db.init_app(app)
+
+    csrf.init_app(app)
+
+    # Register routes
+    from .routes import main
+
+    app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
